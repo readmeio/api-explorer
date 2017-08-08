d9315ae14ecf11c795c9b33f2ddae44abc067c7c
Dom Harrington <dom@harrington-mail.com>
2017-08-08 16:35:40 -0700

    Remove oas conversion server from example

--- example/index.jsx
+++ example/index.jsx
@@ -2,21 +2,12 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const refParser = require('json-schema-ref-parser');
 
-const swagger = require('../packages/api-explorer-ui/__tests__/fixtures/petstore/swagger');
+const oas = require('../packages/api-explorer-ui/__tests__/fixtures/petstore/oas');
 const createDocs = require('../packages/api-explorer-ui/lib/create-docs');
 
 const ApiExplorer = require('../packages/api-explorer-ui');
 
 (async function main() {
-  const oas = await fetch('http://localhost:4000', {
-    method: 'post',
-    body: JSON.stringify(swagger),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    mode: 'cors',
-  }).then(response => response.json());
-
   const docs = createDocs(oas, 'api-setting');
   const oasFiles = { 'api-setting': await refParser.dereference(oas) };
 
