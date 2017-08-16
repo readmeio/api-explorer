2d90681d1182bc7799f9d0625c82d7260c8c7784
Dom Harrington <dom@harrington-mail.com>
2017-08-16 15:34:16 -0700

    Add oas.prepareSecurity for AuthBox

--- packages/api-explorer-ui/__tests__/lib/Oas.test.js
+++ packages/api-explorer-ui/__tests__/lib/Oas.test.js
@@ -59,3 +59,71 @@ test('should be able to access properties on oas', () => {
     info: { version: '1.0' },
   }).info.version).toBe('1.0');
 });
+
+// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#securitySchemeObject
+describe('prepareSecurity()', () => {
+  const path = '/auth';
+  const method = 'get';
+
+  function createSecurityOas(schemes) {
+    // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#securityRequirementObject
+    const security = Object.keys(schemes).reduce((prev, scheme) => {
+      return Object.assign(prev, { [scheme]: [] });
+    }, {});
+
+    return new Oas({
+      components: { securitySchemes: schemes },
+      paths: {
+        [path]: {
+          [method]: { security },
+        },
+      },
+    });
+  }
+
+  test('http: should return with a type of Basic', () => {
+    const oas = createSecurityOas({
+      securityScheme: {
+        type: 'http',
+        scheme: 'basic',
+      },
+    });
+
+    expect(oas.prepareSecurity(path, method)).toEqual({
+      Basic: [oas.components.securitySchemes.securityScheme],
+    });
+  });
+
+  test('apiKey/query: should return with a type of Query', () => {
+    const oas = createSecurityOas({
+      securityScheme: {
+        type: 'apiKey',
+        in: 'query',
+      },
+    });
+
+    expect(oas.prepareSecurity(path, method)).toEqual({
+      Query: [oas.components.securitySchemes.securityScheme],
+    });
+  });
+
+  test('apiKey/header: should return with a type of Header', () => {
+    const oas = createSecurityOas({
+      securityScheme: {
+        type: 'apiKey',
+        in: 'header',
+      },
+    });
+
+    expect(oas.prepareSecurity(path, method)).toEqual({
+      Header: [oas.components.securitySchemes.securityScheme],
+    });
+  });
+
+  test('should set a `key` property');
+
+  // TODO We dont currently support cookies?
+  test('apiKey/cookie: should return with a type of Cookie');
+
+  test('should throw if attempting to use a non-existent scheme');
+});
--- packages/api-explorer-ui/src/lib/Oas.js
+++ packages/api-explorer-ui/src/lib/Oas.js
@@ -6,14 +6,50 @@ class Oas {
   }
 
   hasAuth(path, method) {
-    const operation = getPathOperation(this, { swagger: { path }, api: { method } });
-
-    const security = operation.security || this.security;
+    const security = this.getSecurity(path, method);
     return !!(security && security.length);
   }
 
   getPathOperation(doc) {
     return getPathOperation(this, doc);
+  }
+
+  getSecurity(path, method) {
+    const operation = getPathOperation(this, { swagger: { path }, api: { method } });
+
+    return operation.security || this.security;
+  }
+
+  prepareSecurity(path, method) {
+    const securityRequirements = this.getSecurity(path, method);
+
+    return Object.keys(securityRequirements).map((requirement) => {
+      let security;
+      try {
+        security = this.components.securitySchemes[requirement];
+      } catch (e) {
+        return;
+      }
+
+      if (!security) return;
+      let type = security.type;
+      if (security.type === 'http' && security.scheme === 'basic') {
+        type = 'Basic';
+      } else if (security.type === 'oauth2') {
+        type = 'OAuth2';
+      } else if (security.type === 'apiKey' && security.in === 'query') {
+        type = 'Query';
+      } else if (security.type === 'apiKey' && security.in === 'header') {
+        type = 'Header';
+      }
+
+      security._key = requirement;
+      return { type, security };
+    }).reduce((prev, next) => {
+      if (!prev[next.type]) prev[next.type] = [];
+      prev[next.type].push(next.security);
+      return prev;
+    }, {});
   }
 }
 
