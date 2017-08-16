bfd28a26c1da060a9494a6f26783c84875d2bfd1
Dom Harrington <dom@harrington-mail.com>
2017-08-16 10:47:26 -0700

    Fix tests

--- example/index.jsx
+++ example/index.jsx
@@ -5,7 +5,7 @@ const refParser = require('json-schema-ref-parser');
 const oas = require('../packages/api-explorer-ui/__tests__/fixtures/petstore/oas');
 const createDocs = require('../packages/api-explorer-ui/lib/create-docs');
 
-const ApiExplorer = require('../packages/api-explorer-ui');
+const ApiExplorer = require('../packages/api-explorer-ui/src/index.jsx');
 
 (async function main() {
   const docs = createDocs(oas, 'api-setting');
