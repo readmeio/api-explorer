622346770179d27973b124f3ecc4e03515613489
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-09-12 15:02:23 -0700

    Switch tabs on code test complete

--- packages/api-explorer-ui/__tests__/block-types/Code.test.jsx
+++ packages/api-explorer-ui/__tests__/block-types/Code.test.jsx
@@ -26,4 +26,31 @@ describe('Code', () => {
     const codeInput = mount(<Code block={block} />);
     expect(codeInput.find('em').text()).toBe('test');
   });
+
+  test('Code should switch tabs', () => {
+    const block = {
+      type: 'code',
+      sidebar: undefined,
+      data: {
+        codes: [
+          {
+            code: 'whjdwhjwejhkwhjk',
+            language: 'text',
+            status: 400,
+            name: 'test',
+          },
+          {
+            code: 'var a = 1',
+            language: 'javascript',
+          },
+        ],
+      },
+    };
+    const codeInput = mount(<Code block={block} opts={{}} />);
+
+    const anchor = codeInput.find('li a').at(1);
+
+    anchor.simulate('click', { preventDefault: () => {} });
+    expect(anchor.hasClass('active')).toBe(true);
+  });
 });
