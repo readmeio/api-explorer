2d02cffd5097591c1d475fc37d36e6c8b8bc0755
Dom Harrington <dom@harrington-mail.com>
2017-09-13 13:29:27 -0700

    Add more apiheader tests

--- packages/api-explorer-ui/__tests__/block-types/ApiHeader.test.jsx
+++ packages/api-explorer-ui/__tests__/block-types/ApiHeader.test.jsx
@@ -6,12 +6,24 @@ describe('ApiHeader', () => {
   test('Api Header will render text in table header cells', () => {
     const block = {
       type: 'api-header',
-      sidebar: undefined,
       data: {
         title: 'This is header',
       },
     };
-    const apiHeaderInput = shallow(<ApiHeader block={block} />);
-    expect(apiHeaderInput.find('h1').text()).toBe('This is header');
+    const apiHeader = shallow(<ApiHeader block={block} />);
+    expect(apiHeader.find('h1').text()).toBe('This is header');
+  });
+
+  test('should render with the type in a span', () => {
+    const type = 'post';
+    const block = {
+      type: 'api-header',
+      data: {
+        title: 'This is header',
+        type,
+      },
+    };
+    const apiHeader = shallow(<ApiHeader block={block} />);
+    expect(apiHeader.find(`span.type-${type}`).length).toBe(1);
   });
 });
