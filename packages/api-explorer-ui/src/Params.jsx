d1766c7544a391bbf78f8e7893fc16bc06c148d6
Dom Harrington <dom@harrington-mail.com>
2017-09-13 20:16:11 -0700

    Progress towards form styling

--- packages/api-explorer-ui/src/Params.jsx
+++ packages/api-explorer-ui/src/Params.jsx
@@ -4,29 +4,92 @@ const Form = require('react-jsonschema-form').default;
 const UpDownWidget = require('react-jsonschema-form/lib/components/widgets/UpDownWidget').default;
 const TextWidget = require('react-jsonschema-form/lib/components/widgets/TextWidget').default;
 
+// const TitleField = require('./form-components/TitleField');
+// const ObjectField = require('./form-components/ObjectField');
+
 const Oas = require('./lib/Oas');
 
 const { Operation } = Oas;
 const parametersToJsonSchema = require('./lib/parameters-to-json-schema');
+
+// const REQUIRED_FIELD_SYMBOL = '*';
+
+// function Label(props) {
+//   const { label, required, id } = props;
+//   if (!label) {
+//     // See #312: Ensure compatibility with old versions of React.
+//     return <div />;
+//   }
+//   return (
+//     <label className="control-label" htmlFor={id}>
+//       {required ? label + REQUIRED_FIELD_SYMBOL : label}
+//     </label>
+//   );
+// }
+
+// function CustomFieldTemplate(props) {
+//   const {
+//     id,
+//     classNames,
+//     label,
+//     children,
+//     errors,
+//     help,
+//     description,
+//     hidden,
+//     required,
+//     displayLabel,
+//   } = props;
+//   if (hidden) {
+//     return children;
+//   }
+
+//   return (
+//     <div className={classNames}>
+//       {displayLabel && <Label label={label} required={required} id={id} />}
+//       {displayLabel && description ? description : null}
+//       {children}
+//       {errors}
+//       {help}
+//     </div>
+//   );
+// }
 
 function Params({ oas, operation, formData, onChange }) {
   const jsonSchema = parametersToJsonSchema(operation, oas);
 
   return (
     <div className="api-manager">
-      {jsonSchema && (
-        <Form
-          id={`form-${operation.operationId}`}
-          schema={jsonSchema}
-          widgets={{ int64: UpDownWidget, int32: UpDownWidget, uuid: TextWidget }}
-          // eslint-disable-next-line no-console
-          onSubmit={form => console.log('submit', form.formData)}
-          formData={formData}
-          onChange={form => onChange(form.formData)}
-        >
-          <button type="submit" style={{ display: 'none' }} />
-        </Form>
-      )}
+      <div className="param-table">
+        {jsonSchema &&
+          jsonSchema.map(schema => {
+            return (
+              <div key={schema.type}>
+                <div className="param-header">
+                  <h3>{schema.label}</h3>
+                  <div className="param-header-border" />
+                </div>
+                <Form
+                  id={`form-${operation.operationId}`}
+                  schema={schema.schema}
+                  widgets={{ int64: UpDownWidget, int32: UpDownWidget, uuid: TextWidget }}
+                  // eslint-disable-next-line no-console
+                  onSubmit={form => console.log('submit', form.formData)}
+                  formData={formData[schema.type]}
+                  onChange={form => {
+                    // return onChange({ [schema.type]: { $set: form.formData } })
+                    return onChange({ [schema.type]: form.formData })
+                  }}
+                  // FieldTemplate={CustomFieldTemplate}
+                  // fields={{ObjectField, TitleField}}
+                  className="param-table"
+                >
+                  <button type="submit" style={{ display: 'none' }} />
+                </Form>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- packages/api-explorer-ui/src/lib/parameters-to-json-schema.js
+++ packages/api-explorer-ui/src/lib/parameters-to-json-schema.js
@@ -50,19 +50,56 @@ module.exports = (pathOperation, oas) => {
   function getBodyParam() {
     const schema = getSchema(pathOperation);
 
-    if (!schema) return {};
+    if (!schema) return null;
 
-    return { body: Object.assign({ description: types.body }, schema) };
+    return {
+      type: 'body',
+      label: types.body,
+      schema,
+    };
   }
 
-  return {
-    type: 'object',
-    definitions: oas.components ? { components: oas.components } : {},
-    properties: Object.assign(
-      getBodyParam(),
-      (pathOperation.parameters || []).reduce(combineParameters, {}),
-    ),
-  };
+  function getOtherParams() {
+    return Object.keys(types).map(type => {
+      const required = [];
+      const parameters = pathOperation.parameters.filter(param => param.in === type);
+      if (parameters.length === 0) return null;
+
+      const properties = parameters.reduce((prev, current) => {
+        const schema = {
+          type: 'string',
+          description: current.description || null,
+        };
+
+        if (current.schema) {
+          if (current.schema.type === 'array') {
+            schema.type = 'array';
+            schema.items = current.schema.items;
+          }
+        }
+
+        prev[current.name] = schema;
+
+        if (current.required) {
+          required.push(current.name);
+        }
+
+        return prev;
+      }, {});
+
+      return {
+        type,
+        label: types[type],
+        schema: {
+          type: 'object',
+          properties,
+          required,
+        },
+      };
+    })
+  }
+
+  return [getBodyParam()].concat(...getOtherParams()).filter(Boolean);
 };
 
 // Exported for use in oas-to-har for default values object
