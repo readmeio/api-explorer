8843177da66820a0bba9c367133097ad62bdaf7a
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-09-28 15:19:22 -0700

    Install fetch-har

--- packages/api-explorer-ui/src/Params.jsx
+++ packages/api-explorer-ui/src/Params.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const Form = require('react-jsonschema-form').default;
-// eslint-disable-next-line import/no-extraneous-dependencies
 const fetchHar = require('fetch-har');
 const UpDownWidget = require('react-jsonschema-form/lib/components/widgets/UpDownWidget').default;
 const TextWidget = require('react-jsonschema-form/lib/components/widgets/TextWidget').default;
