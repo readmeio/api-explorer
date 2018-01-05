6e123a3affca76a9d1cc8e3ab4b3778904f9fc5f
Dom Harrington <dom@harrington-mail.com>
2018-01-04 16:37:16 -0800

    Fix circle ci on gh-pages branch

--- build.sh
+++ build.sh
@@ -13,3 +13,7 @@ cp example/*.css example/dist
 cp -R example/fonts example/dist
 cp -R example/img example/dist
 cp -R example/swagger-files example/dist
+
+# Need to copy over the circleci config so that
+# it picks up we want to ignore the gh-pages branch
+cp -R .circleci example/dist
