1eddf83ed1950384703d9dc51731e84c86c50946
Dom Harrington <dom@harrington-mail.com>
2018-02-08 12:01:57 -0800

    Fix test

--- packages/api-explorer/__tests__/form-components/CheckboxWidget.test.jsx
+++ packages/api-explorer/__tests__/form-components/CheckboxWidget.test.jsx
@@ -3,9 +3,14 @@ const { shallow } = require('enzyme');
 const Checkbox = require('../../src/form-components/CheckboxWidget');
 
 test('should not render description', () => {
-  const props = {
-    label: 'test',
-  };
-  const checkbox = shallow(<Checkbox props={props} />);
-  expect(checkbox.render().html()).toMatchSnapshot();
+  const checkbox = shallow(
+    <Checkbox
+      id="id"
+      label="label"
+      required
+      onChange={() => {}}
+      schema={{ description: 'description' }}
+    />,
+  );
+  expect(checkbox.html()).not.toMatch(/description/);
 });
