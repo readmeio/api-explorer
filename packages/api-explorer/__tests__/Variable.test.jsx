3264da2275871cce6f69835a6bfe69cd6be35ed5
Dom Harrington <dom@harrington-mail.com>
2018-06-22 12:03:25 -0700

    Add slightly better styling to dropdown

--- packages/api-explorer/__tests__/Variable.test.jsx
+++ packages/api-explorer/__tests__/Variable.test.jsx
@@ -3,62 +3,68 @@ const { shallow } = require('enzyme');
 
 const Variable = require('../src/Variable');
 
-test('should render variable', () => {
-  const variable = shallow(<Variable value="123456" />);
+describe('single variable', () => {
+  test('should render value', () => {
+    const variable = shallow(<Variable value="123456" />);
 
-  expect(variable.text()).toBe('123456');
+    expect(variable.text()).toBe('123456');
+  });
+
+  test('should render auth dropdown if default and oauth enabled');
 });
 
-test('should render the first of multiple values', () => {
-  const variable = shallow(
-    <Variable
-      k="apiKey"
-      value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
-    />,
-  );
+describe('multiple variables', () => {
+  test('should render the first of multiple values', () => {
+    const variable = shallow(
+      <Variable
+        k="apiKey"
+        value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
+      />,
+    );
 
-  expect(variable.text()).toBe('123');
+    expect(variable.text()).toBe('123');
+  });
+
+  test('should render whatever the selected name is', () => {
+    const variable = shallow(
+      <Variable
+        k="apiKey"
+        value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
+        selected="project2"
+      />,
+    );
+
+    expect(variable.text()).toBe('456');
+  });
+
+  test('should show dropdown when clicked', () => {
+    const variable = shallow(
+      <Variable
+        k="apiKey"
+        value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
+        selected="project2"
+      />,
+    );
+
+    variable.find('.variable-underline').simulate('click');
+
+    expect(variable.find('ul li').map(el => el.text())).toEqual(['project1', 'project2']);
+  });
+
+  test('should select value when clicked', () => {
+    const variable = shallow(
+      <Variable
+        k="apiKey"
+        value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }, { name: 'project3'}]}
+        selected="project1"
+      />,
+    );
+
+    variable.find('.variable-underline').simulate('click');
+    variable.find('ul li').at(1).simulate('click', { target: { innerText: variable.find('ul li').at(1).text() } });
+
+    expect(variable.text()).toBe('456');
+  });
+
+  test('should render auth dropdown if default and oauth enabled');
 });
-
-test('should render whatever the selected name is', () => {
-  const variable = shallow(
-    <Variable
-      k="apiKey"
-      value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
-      selected="project2"
-    />,
-  );
-
-  expect(variable.text()).toBe('456');
-});
-
-test('should show dropdown when clicked', () => {
-  const variable = shallow(
-    <Variable
-      k="apiKey"
-      value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }]}
-      selected="project2"
-    />,
-  );
-
-  variable.simulate('click');
-
-  expect(variable.find('ul li').map(el => el.text())).toEqual(['project1', 'project2']);
-});
-
-test('should select value when clicked', () => {
-  const variable = shallow(
-    <Variable
-      k="apiKey"
-      value={[{ name: 'project1', apiKey: '123' }, { name: 'project2', apiKey: '456' }, { name: 'project3'}]}
-      selected="project1"
-    />,
-  );
-
-  variable.simulate('click');
-  variable.find('ul li').at(1).simulate('click', { target: { innerText: variable.find('ul li').at(1).text() } });
-
-  expect(variable.text()).toBe('456');
-});
-
-test('should render auth dropdown if default');
--- packages/api-explorer/src/Variable.jsx
+++ packages/api-explorer/src/Variable.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 const React = require('react');
+const classNames = require('classnames');
 
 class Variable extends React.Component {
   constructor(props) {
@@ -7,6 +9,7 @@ class Variable extends React.Component {
 
     this.showDropdown = this.showDropdown.bind(this);
     this.selectValue = this.selectValue.bind(this);
+    this.renderDropdown = this.renderDropdown.bind(this);
   }
   showDropdown() {
     this.setState({ showDropdown: true });
@@ -14,20 +17,49 @@ class Variable extends React.Component {
   selectValue(event) {
     this.setState({ selected: event.target.innerText, showDropdown: false });
   }
+  renderDropdown() {
+    return (
+      <div
+        className={classNames(
+          'ns-popover-dropdown-theme',
+          'ns-popover-bottom-placement',
+          'ns-popover-right-align',
+        )}
+        style={{ position: 'absolute' }}
+      >
+        <div id="variableDropdown" className="ns-popover-tooltip">
+          <div className="ns-triangle" />
+          <div className="triangle" />
+          <ul>
+            {this.props.value.map(key => (
+              <li
+                className={classNames({ active: this.state.selected === key.name })}
+                onClick={this.selectValue}
+                key={key.name}
+              >
+                {key.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
   render() {
     const { k, value } = this.props;
 
     const { selected } = this.state;
 
-    if (this.state.showDropdown) {
-      return (
-        <ul>{value.map(key => <li onClick={this.selectValue} key={key.name}>{key.name}</li>)}</ul>
-      );
-    }
-
     if (Array.isArray(value)) {
       const selectedValue = selected ? value.find(key => key.name === selected) : value[0];
-      return <span onClick={this.showDropdown}>{selectedValue[k]}</span>;
+      return (
+        <span>
+          <span className="variable-underline" onClick={this.showDropdown}>
+            {selectedValue[k]}
+          </span>
+          {this.state.showDropdown && this.renderDropdown()}
+        </span>
+      );
     }
     return <span>{value}</span>;
   }
