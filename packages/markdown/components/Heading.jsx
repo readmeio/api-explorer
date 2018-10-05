66404555290036b493092d04b26a09de2da98ead
MEHMET BARAN (OUTSOURCE) <mehmetbaran@mehmetbaran.net>
2018-10-05 08:52:24 +0300

    This closes #159, fixed generating div id of a markdown header when header is styled with special markdown characters

--- packages/markdown/components/Heading.jsx
+++ packages/markdown/components/Heading.jsx
@@ -2,13 +2,24 @@ const React = require('react');
 const PropTypes = require('prop-types');
 
 function Heading(props) {
-  const id = `section-${props.children[0].toLowerCase().replace(/[^\w]+/g, '-')}`;
+  // const id = `section-${props.children[0].toLowerCase().replace(/[^\w]+/g, '-')}`;
+  const id = `section-${generateId(props.children[0])}`;
   return React.createElement(props.level, { className: 'header-scroll' }, [
     <div className="anchor waypoint" id={id} key={`anchor-${id}`} />,
     ...props.children,
     // eslint-disable-next-line jsx-a11y/anchor-has-content
     <a className="fa fa-anchor" href={`#${id}`} key={`anchor-icon-${id}`} />,
   ]);
+}
+
+function generateId(element) {
+  if(typeof element == "object") {
+    const type = (element.type.name || element.type).toLowerCase();
+    return type + `-${generateId(element.props.children[0])}`;
+  }
+  else {
+    return element.toLowerCase().replace(/[^\w]+/g, '-');
+  } 
 }
 
 function createHeading(level) {
