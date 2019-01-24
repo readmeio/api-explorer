5c4229a9f3f462986f0cf307fd3ef83f04a68583
Dom Harrington <domharrington@protonmail.com>
2019-01-24 15:48:33 -0800

    Temp fix tests in AuthBox

--- packages/api-explorer/__tests__/AuthBox.test.jsx
+++ packages/api-explorer/__tests__/AuthBox.test.jsx
@@ -97,8 +97,10 @@ test("should work for Basic (Basic has it's own state)", () => {
   basic.inputChange('user', 'user');
   basic.inputChange('password', 'password');
 
-  expect(onChange.mock.calls[0][0]).toEqual({ auth: { basic: { user: 'user', password: '' } } });
-  expect(onChange.mock.calls[1][0]).toEqual({
+  // TODO this should be removed. See comment above componentDidMount in Basic
+  expect(onChange.mock.calls[0][0]).toEqual({ auth: { basic: { user: '', password: '' } } });
+  expect(onChange.mock.calls[1][0]).toEqual({ auth: { basic: { user: 'user', password: '' } } });
+  expect(onChange.mock.calls[2][0]).toEqual({
     auth: { basic: { user: 'user', password: 'password' } },
   });
 });
