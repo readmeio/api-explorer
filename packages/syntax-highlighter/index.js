28c50bcd75bd1dca0ec29a410b689e50c91e0971
Riccardo Di Benedetto <riccardo.dibenedetto@mia-platform.eu>
2019-04-29 16:49:53 +0200

    minor fix

--- packages/syntax-highlighter/index.js
+++ packages/syntax-highlighter/index.js
@@ -2,13 +2,12 @@ const React = require('react');
 const codemirror = require('./codemirror.jsx');
 
 module.exports = (code, lang, opts = { dark: false, tokenizeVariables: false }) =>{
-  const codeMirrored = codemirror(code, lang, opts)
   return React.createElement(
     'span',
     {
       className: opts.dark ? 'cm-s-tomorrow-night' : 'cm-s-neo',
     },
-    codeMirrored,
+    codemirror(code, lang, opts),
   );
   }
 module.exports.uppercase = require('./uppercase');
