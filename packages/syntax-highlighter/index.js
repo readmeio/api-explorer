c5ff0285e99d1dc2f51d04217ce1d785933f6490
Riccardo Di Benedetto <riccardo.dibenedetto@mia-platform.eu>
2019-04-29 16:51:40 +0200

    removed useless return

--- packages/syntax-highlighter/index.js
+++ packages/syntax-highlighter/index.js
@@ -1,13 +1,11 @@
 const React = require('react');
 const codemirror = require('./codemirror.jsx');
 
-module.exports = (code, lang, opts = { dark: false, tokenizeVariables: false }) =>{
-  return React.createElement(
+module.exports = (code, lang, opts = { dark: false, tokenizeVariables: false }) => React.createElement(
     'span',
     {
       className: opts.dark ? 'cm-s-tomorrow-night' : 'cm-s-neo',
     },
     codemirror(code, lang, opts),
   );
-  }
 module.exports.uppercase = require('./uppercase');
