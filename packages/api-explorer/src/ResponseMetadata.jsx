6c38affeadb78bc2435acebceeeb02ea222e9327
Federico Maggi <federico.maggi@mia-platform.eu>
2019-04-30 11:03:21 +0200

    handle i18n in ResponseMetadata component

--- packages/api-explorer/src/ResponseMetadata.jsx
+++ packages/api-explorer/src/ResponseMetadata.jsx
@@ -1,3 +1,5 @@
+import {injectIntl} from 'react-intl';
+
 const React = require('react');
 const IconStatus = require('./IconStatus');
 const PropTypes = require('prop-types');
@@ -19,43 +21,46 @@ Meta.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-function ResponseMetadata({ result }) {
+function ResponseMetadata({ result, intl }) {
   return (
     <div
       className="hub-reference-results-meta tabber-body-metadata tabber-body"
       style={{ display: 'block' }}
     >
-      <Meta label="Method">
+      <Meta label={intl.formatMessage({id: "api.metadata.method", defaultMessage: "Method"})}>
         <div>{result.method.toString()}</div>
       </Meta>
 
-      <Meta label="URL">
+      <Meta label={intl.formatMessage({id: "api.metadata.url", defaultMessage: "URL"})}>
         <div>{result.url}</div>
       </Meta>
 
-      <Meta label="Request Headers">
+      <Meta label={intl.formatMessage({id: "api.metadata.requestheaders", defaultMessage: "Request Headers"})}>
         <pre>{result.requestHeaders.join('\n')}</pre>
       </Meta>
 
-      <Meta label="Request Data">
+      <Meta label={intl.formatMessage({id: "api.metadata.requestbody", defaultMessage: "Request Data"})}>
         <pre>{result.requestBody}</pre>
       </Meta>
 
-      <Meta label="Status">
+      <Meta label={intl.formatMessage({id: "api.metadata.status", defaultMessage: "Status"})}>
         <span className="httpstatus">
           <IconStatus status={result.status} />
         </span>
       </Meta>
 
-      <Meta label="Response Headers">
+      <Meta label={intl.formatMessage({id: "api.metadata.responseheaders", defaultMessage: "Response Headers"})}>
         <pre>{result.responseHeaders.join('\n')}</pre>
       </Meta>
     </div>
   );
 }
 
-module.exports = ResponseMetadata;
+module.exports = injectIntl(ResponseMetadata);
 
 ResponseMetadata.propTypes = {
   result: PropTypes.shape({}).isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }).isRequired,
 };
