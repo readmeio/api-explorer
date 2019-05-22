readmeio/api-explorer

commit 2d35baefea8f1a972b9577b01bb66ba6d0b11b31

fix current bugs - code location, add test

packages/api-explorer/src/form-components/SchemaField.jsx
@@ -9,21 +9,8 @@ function getDefaultNumFormat(type) {
   return '';
 }
 
-function doesFormatExist(type, format) {
-  const availableFormats = [
-    'int64',
-    'int32',
-    'double',
-    'float',
-    'binary',
-    'byte',
-    'string',
-    'uuid',
-    'duration',
-    'dateTime',
-    'integer',
-    'json',
-  ];
+function doesFormatExist(widgets, type, format) {
+  const availableFormats = Object.keys(widgets);
 
   return type === 'string' && availableFormats.includes(format);
 }
@@ -47,7 +34,8 @@ function getCustomType(schema) {
 }
 
 function SchemaField(props) {
-  if (!doesFormatExist(props.schema.type, props.schema.format)) props.schema.format = undefined;
+  if (!doesFormatExist(props.registry.widgets, props.schema.type, props.schema.format))
+    props.schema.format = undefined;
 
   if (props.schema.readOnly) {
     // Maybe use this when it's been merged?
@@ -84,6 +72,9 @@ SchemaField.propTypes = {
     format: PropTypes.string,
     enumNames: PropTypes.array,
     readOnly: PropTypes.bool,
+  }).isRequired,
+  registry: PropTypes.shape({
+    widgets: PropTypes.object,
   }).isRequired,
   uiSchema: PropTypes.shape({}),
 };

packages/api-explorer/__tests__/Params.test.jsx
@@ -62,6 +62,17 @@ test('boolean should render as <select>', () => {
   expect(select.find('option').map(el => el.text())).toEqual(['', 'true', 'false']);
 });
 
+test('should strip out unknown format for string type', () => {
+  const params = mount(
+    <div>
+      <Params {...props} operation={oas.operation('/pet/{petId}', 'post')} />
+    </div>,
+  );
+
+  expect(params.find('input[label="unknown format"]').length).toBe(1);
+  expect(params.find('input[format="unknown format"]').length).toBe(0);
+});
+
 const jsonOperation = new Operation(oas, '/path', 'post', {
   requestBody: {
     content: {

commit 8b69753713408ab7a35885b973fa842cab571170

Fix regression introduced in #202
This caused some formats to get removed, which meant that for integers
we were setting the default `int32` instead of `int64`.

We did have tests for this, but because the module was mutating the data
we were testing against the broken schema!

https://github.com/readmeio/api-explorer/pull/202

packages/api-explorer/src/form-components/SchemaField.jsx
@@ -12,7 +12,7 @@ function getDefaultNumFormat(type) {
 function doesFormatExist(widgets, type, format) {
   const availableFormats = Object.keys(widgets);
 
-  return type === 'string' && availableFormats.includes(format);
+  return typeof type === 'string' && availableFormats.includes(format);
 }
 
 function isNumType(schema, type, format) {

packages/api-explorer/__tests__/Params.test.jsx
@@ -178,10 +178,11 @@ function renderParams(schema, customProps) {
 }
 
 function testNumberClass(schema) {
+  const clonedSchema = JSON.parse(JSON.stringify(schema))
   test(`${JSON.stringify(schema)} should have correct class`, () => {
     const params = renderParams(schema);
 
-    expect(params.find(`.field-${schema.type}.field-${schema.format}`).length).toBe(1);
+    expect(params.find(`.field-${clonedSchema.type}.field-${clonedSchema.format}`).length).toBe(1);
   });
 }
 