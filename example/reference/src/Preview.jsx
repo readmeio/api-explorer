fa557e1566351de2a274707cda49ffbb9b7bc103
Jon Ursenbach <erunion@users.noreply.github.com>
2019-07-26 11:01:17 -0700

    Redesigning the error and loading states in the reference demo (#226)
    * Capitalizing "uploads an image" in the Petstore OAS so it's consistent.
    
    * Slight redesign of error and loading states in the reference demo.
    
    * Adding error catching to the swagger->oai conversion.
    
    * Cleaning up error states in the preview site.

--- example/reference/src/Preview.jsx
+++ example/reference/src/Preview.jsx
@@ -14,33 +14,67 @@ class Preview extends React.Component {
     const searchParams = new URLSearchParams(window.location.search);
     this.props.fetchSwagger(searchParams.get('url') || '../swagger-files/petstore.json');
   }
+
+  componentDidUpdate() {
+    if (!this.props.isLoading) {
+      // Introduce some artifical loading so if we have a bad spec cached, we don't flash a loading
+      // screen upon the user for a split second.
+      setTimeout(() => {
+        document.getElementById('main').classList.add('is-loaded');
+        document.getElementById('loading-screen').classList.add('is-loaded');
+        document.getElementById('hub-sidebar-parent').display = 'none';
+
+        if (this.props.status.length) {
+          document.getElementById('hub-content').classList.add('with-errors');
+        }
+      }, 250);
+    }
+  }
+
   render() {
-    const { status, docs, oas, oauth } = this.props;
+    const { status, docs, invalidSpec, invalidSpecPath, oas, oauth } = this.props;
 
     return (
       <div>
         <div id="hub-reference">
           <div className="hub-reference">
-            <div className="hub-reference-section">
-              <div className="hub-reference-left">
-                <div className="hub-preview">
-                  <div className="hub-preview-owlbert" /><i className="icon icon-readme" />
-                  <p><strong>Like what you see?</strong> ReadMe makes it easy to create beautiful documentation for your API! You can import a Swagger/OAS file&hellip; but that&apos;s not all! ReadMe also helps you build a community, document non-API references and much more! Sign up now to get awesome docs for your Swagger/OAS file!</p><a className="btn btn-primary btn-lg" href="https://dash.readme.io/signup">Sign Up for ReadMe</a>
-                </div>
-              </div>
-              <div className="hub-reference-right">&nbsp;</div>
-            </div>
-
-            {
-              status.length ? (
+            {!status.length
+              ? (
                 <div className="hub-reference-section">
                   <div className="hub-reference-left">
-                    <pre style={{ marginLeft: '20px' }}>{status.join('\n')}</pre>
+                    <div className="hub-preview">
+                      <div className="hub-preview-owlbert" /><i className="icon icon-readme" />
+                      <p><strong>Like what you see?</strong> ReadMe makes it easy to create beautiful documentation for your API! You can import a Swagger/OAS file&hellip; but that&apos;s not all! ReadMe also helps you build a community, document non-API references and much more! Sign up now to get awesome docs for your Swagger/OAS file!</p><a className="btn btn-primary btn-lg" href="https://dash.readme.io/signup">Sign Up for ReadMe</a>
+                    </div>
                   </div>
                   <div className="hub-reference-right">&nbsp;</div>
                 </div>
-              ) : null
-            }
+              ) : (
+                <div className="hub-reference-section">
+                  <div id="hub-reference-loading-error">
+                    <div className="hub-reference-loading-error-box">
+                      {invalidSpec ? (
+                        <div>
+                          <h3 className="hub-reference-loading-error-header">
+                            Invalid API Specification
+                          </h3>
+                          <p>{invalidSpec}</p>
+                          <p>
+                            <a className="btn btn-primary btn-lg" href={`http://openap.is/validate?url=${invalidSpecPath}`}>View Validation Report</a>
+                          </p>
+                        </div>
+                      ) : (
+                        <div>
+                          <h3 className="hub-reference-loading-error-header">
+                            Couldn&apos;t be loaded
+                          </h3>
+                          <p>There was an error fetching your API Specification.</p>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              )}
           </div>
         </div>
 
@@ -70,6 +104,9 @@ class Preview extends React.Component {
 }
 
 Preview.propTypes = {
+  invalidSpec: PropTypes.string,
+  invalidSpecPath: PropTypes.string,
+  isLoading: PropTypes.bool,
   oauth: PropTypes.bool,
   oas: PropTypes.shape({}).isRequired,
   docs: PropTypes.arrayOf(PropTypes.shape).isRequired,
@@ -78,6 +115,9 @@ Preview.propTypes = {
 };
 
 Preview.defaultProps = {
+  invalidSpec: null,
+  invalidSpecPath: null,
+  isLoading: true,
   oauth: false,
 };
 
