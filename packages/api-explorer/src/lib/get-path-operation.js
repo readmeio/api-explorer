531536cef0a40568ca6e6ce8422bdb939f97c5b0
Sean Dokko <dokko1230@gmail.com>
2019-07-30 16:29:55 -0700

    ♻️ call getPath only once

--- packages/api-explorer/src/lib/get-path-operation.js
+++ packages/api-explorer/src/lib/get-path-operation.js
@@ -1,8 +1,9 @@
 const getPath = require('./get-path');
 
 module.exports = function getPathOperation(swagger, doc) {
-  if (swagger.paths && doc.swagger && getPath(swagger, doc)) {
-    return getPath(swagger, doc)[doc.api.method];
+  if (swagger.paths && doc.swagger) {
+    const path = getPath(swagger, doc);
+    if (path) return path[doc.api.method];
   }
 
   return { parameters: [] };
