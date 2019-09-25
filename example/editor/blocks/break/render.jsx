0e2a39a96a3014144339863dc3555839b0cad034
Rafe Goldberg <rafegoldberg@gmail.com>
2019-09-25 13:41:31 -0700

    restore default example
    delete editor working files

--- example/editor/blocks/break/render.jsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-
-export default ({ attributes, children, node }) => <hr {...attributes} />;
--- example/editor/marks/deleted.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-
-export default function Deleted(props) {
-  return <del>{props.children}</del>;
-};
