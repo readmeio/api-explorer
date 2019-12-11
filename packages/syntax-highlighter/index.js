218a5ae5f3a0a647920584ea18ad4938662d5f88
Rafe Goldberg <rafegoldberg@gmail.com>
2019-12-10 18:02:13 -0800

    revert unnecessary changes

--- packages/syntax-highlighter/index.js
+++ packages/syntax-highlighter/index.js
@@ -1,15 +1,14 @@
 const React = require('react');
 const codemirror = require('./codemirror.jsx');
 
-const SyntaxHighlighter = (code, lang, opts = { dark: false, tokenizeVariables: false }) => {
-  return React.createElement(
+// eslint-disable-next-line react/display-name
+module.exports = (code, lang, opts = { dark: false, tokenizeVariables: false }) =>
+  React.createElement(
     'span',
     {
       className: opts.dark ? 'cm-s-tomorrow-night' : 'cm-s-neo',
     },
     codemirror(typeof code === 'string' ? code : '', lang, opts),
   );
-};
 
-module.exports = SyntaxHighlighter;
 module.exports.uppercase = require('./uppercase');
