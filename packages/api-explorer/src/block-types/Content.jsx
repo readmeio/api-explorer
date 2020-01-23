eb5a67cd2a025f2ac99e97c8d9a6ba0c8ef58e7d
Rafe Goldberg <rafegoldberg@gmail.com>
2020-01-23 11:02:51 -0800

    nix comment

--- packages/api-explorer/src/block-types/Content.jsx
+++ packages/api-explorer/src/block-types/Content.jsx
@@ -1,11 +1,6 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-/** To make this work in a PR app you need to
- * 'flip' these two lines and npm install the
- *  readmeio/api-explorer#beta/editor-hacking
- *  GitHub branch. Hey! If it works, it works!
- */
 const markdown = require('@readme/markdown').default;
 
 const Content = props => {
