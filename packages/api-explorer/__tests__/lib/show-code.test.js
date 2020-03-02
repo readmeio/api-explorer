188cb9ebc477861653dbd125e5a365934c0dc643
Jon Ursenbach <jon@ursenba.ch>
2020-03-02 10:47:22 -0800

    test: updating some obtuse assertions

--- packages/api-explorer/__tests__/lib/show-code.test.js
+++ packages/api-explorer/__tests__/lib/show-code.test.js
@@ -2,12 +2,12 @@ const extensions = require('@readme/oas-extensions');
 
 const showCode = require('../../src/lib/show-code');
 
-test.skip('should return true if there are examples', () => {});
+test.todo('should return true if there are examples');
 
-test.skip('should return true if there are results', () => {});
+test.todo('should return true if there are results');
 
 test('should return true if it has try it now', () => {
   expect(showCode({ [extensions.EXPLORER_ENABLED]: true }, {})).toBe(true);
 });
 
-test.skip('should return false otherwise', () => {});
+test.todo('should return false otherwise');
