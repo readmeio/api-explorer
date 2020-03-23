aca7b18545c784200373d12ad4ff36d3d48f8d4c
Sean Dokko <dokko1230@gmail.com>
2020-03-23 13:34:53 -0700

    🐛 create normalize as an option

--- packages/markdown/index.js
+++ packages/markdown/index.js
@@ -87,7 +87,7 @@ export function normalize(blocks) {
   // normalize magic block lines
   // eslint-disable-next-line no-param-reassign
   blocks = blocks
-    .replace(/\[block:/g, '\n[block:')
+    .replace(/\[block:/g, '\n\n[block:')
     .replace(/\[\/block\]/g, '[/block]\n')
     .trim()
     .replace(/^(#+)(.+)/gm, '$1 $2');
@@ -143,7 +143,7 @@ export function plain(text, opts = options) {
       createElement: React.createElement,
       Fragment: React.Fragment,
     })
-    .processSync(text).contents;
+    .processSync(opts.normalize ? normalize(text) : text).contents;
 }
 
 /**
@@ -179,7 +179,7 @@ export function react(text, opts = options) {
         img: Image(sanitize),
       },
     })
-    .processSync(text).contents;
+    .processSync(opts.normalize ? normalize(text) : text).contents;
 }
 
 /**
@@ -188,7 +188,7 @@ export function react(text, opts = options) {
 export function html(text, opts = options) {
   if (!text) return null;
 
-  return parseMarkdown(opts).use(rehypeStringify).processSync(text).contents;
+  return parseMarkdown(opts).use(rehypeStringify).processSync(opts.normalize ? normalize(text) : text).contents;
 }
 
 /**
@@ -196,7 +196,7 @@ export function html(text, opts = options) {
  */
 export function ast(text, opts = options) {
   if (!text) return null;
-  return parseMarkdown(opts).use(remarkStringify, opts.markdownOptions).parse(text);
+  return parseMarkdown(opts).use(remarkStringify, opts.markdownOptions).parse(opts.normalize ? normalize(text) : text);
 }
 
 /**
