c3500abc83b68b75023bae3cda553f984a681c76
Jon Ursenbach <jon@ursenba.ch>
2020-03-25 12:44:57 -0700

    fix: removing some environmental exclusions of styles

--- packages/markdown/index.js
+++ packages/markdown/index.js
@@ -1,12 +1,4 @@
-// There's a bug in jsdom where Jest spits out heaps of errors from it not being able to interpret
-// this file, so let's not include this when running tests since we aren't doing visual testing
-// anyways.
-// https://github.com/jsdom/jsdom/issues/217
-/* istanbul ignore next */
-if (process.env.NODE_ENV !== 'test') {
-  // eslint-disable-next-line global-require
-  require('./styles/main.scss');
-}
+require('./styles/main.scss');
 
 const React = require('react');
 const unified = require('unified');
