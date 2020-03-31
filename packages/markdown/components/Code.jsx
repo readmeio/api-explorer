2356eee69f2b655449a8c29bc1815be0f39fd9b0
Rafe Goldberg <rafegoldberg@gmail.com>
2020-03-31 13:08:19 -0700

    extremely simple server/client fork
    for frontend-only libs (in this case CodeMirror...)

--- packages/markdown/components/Code.jsx
+++ packages/markdown/components/Code.jsx
@@ -1,28 +1,26 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-// const syntaxHighlighter = require('@readme/syntax-highlighter');
 
-class CodeClass extends React.Component {
-  render() {
-    const { className, children, lang, meta } = this.props;
-    const language = (className || '').replace('language-', '');
-    return (
-      <code className={language ? `lang-${language}` : null} data-lang={lang} name={meta}>
-        {children[0]}
-        {/* {syntaxHighlighter(children[0], language, { tokenizeVariables: true })} */}
-      </code>
-    );
-  }
+const syntaxHighlighter = typeof window !== 'undefined' ? require('@readme/syntax-highlighter') : false;
+
+function Code(props) {
+  const { className, children, lang, meta } = props;
+  const language = (className || '').replace('language-', '');
+  return (
+    <code className={language ? `lang-${language}` : null} data-lang={lang} name={meta}>
+      {syntaxHighlighter ? syntaxHighlighter(children[0], language, { tokenizeVariables: true }) : children[0]}
+    </code>
+  );
 }
 
-CodeClass.propTypes = {
+Code.propTypes = {
   children: PropTypes.arrayOf(PropTypes.string).isRequired,
   className: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.string,
 };
 
-CodeClass.defaultProps = {
+Code.defaultProps = {
   className: '',
   lang: '',
   meta: '',
@@ -31,5 +29,5 @@ CodeClass.defaultProps = {
 module.exports = sanitizeSchema => {
   // This is for code blocks class name
   sanitizeSchema.attributes.code = ['className', 'lang', 'meta', 'value'];
-  return CodeClass;
+  return Code;
 };
