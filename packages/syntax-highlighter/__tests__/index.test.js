7d5215441ef6559ecda58a5c09fcf56438ab190e
Jon Ursenbach <erunion@users.noreply.github.com>
2020-04-27 16:05:55 -0700

    feat: adding support for a handful of language highlight aliases (#643)

--- packages/syntax-highlighter/__tests__/index.test.js
+++ packages/syntax-highlighter/__tests__/index.test.js
@@ -54,48 +54,294 @@ test('should keep enclosing characters around the variable', () => {
   expect(mount(syntaxHighlighter("'<<apiKey>>'", 'json', { tokenizeVariables: true })).text()).toBe("'APIKEY'");
 });
 
-describe('specific languages', () => {
-  it('should work for modes with an array like java', () => {
-    expect(shallow(syntaxHighlighter('service = client.service().messaging();', 'java')).html()).toBe(
-      '<span class="cm-s-neo"><span class="cm-variable">service</span> <span class="cm-operator">=</span> <span class="cm-variable">client</span>.<span class="cm-variable">service</span>().<span class="cm-variable">messaging</span>();</span>'
-    );
+// https://github.com/leachim6/hello-world/
+describe('supported languages', () => {
+  it('asp', () => {
+    const code = `<%@ Language= "VBScript" %>
+<%
+  Response.Write("Hello World")
+%>`;
+
+    expect(shallow(syntaxHighlighter(code, 'asp')).html()).toMatchSnapshot();
   });
 
-  it('should work for html', () => {
-    expect(shallow(syntaxHighlighter('<p>test</p>', 'html')).html()).toBe(
-      '<span class="cm-s-neo"><span class="cm-tag cm-bracket">&lt;</span><span class="cm-tag">p</span><span class="cm-tag cm-bracket">&gt;</span>test<span class="cm-tag cm-bracket">&lt;/</span><span class="cm-tag">p</span><span class="cm-tag cm-bracket">&gt;</span></span>'
-    );
+  it('c', () => {
+    const code = `#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}`;
+
+    expect(shallow(syntaxHighlighter(code, 'c')).html()).toMatchSnapshot();
   });
 
-  it('should work for php without opening tag', () => {
-    expect(shallow(syntaxHighlighter('echo "Hello World";', 'php')).html()).toContain('cm-keyword');
+  it('c++', () => {
+    const code = `#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World\n";
+}`;
+
+    const cplusplus = shallow(syntaxHighlighter(code, 'cplusplus')).html();
+
+    expect(cplusplus).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'cpp')).html()).toStrictEqual(cplusplus);
+    expect(shallow(syntaxHighlighter(code, 'c++')).html()).toStrictEqual(cplusplus);
   });
 
-  it('should work for dockerfiles', () => {
-    expect(shallow(syntaxHighlighter('FROM alpine:3.4', 'dockerfile')).html()).toContain('cm-keyword');
+  it('c#', () => {
+    const code = `class HelloWorld {
+  static void Main() {
+    System.Console.WriteLine("Hello World");
+  }
+}`;
+
+    const csharp = shallow(syntaxHighlighter(code, 'csharp')).html();
+
+    expect(csharp).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'cs')).html()).toStrictEqual(csharp);
   });
 
-  it('should work for kotlin', () => {
-    expect(shallow(syntaxHighlighter('println("$index: $element")', 'kotlin')).html()).toContain('cm-variable');
+  it('css flavors', () => {
+    const code = `body::before {
+  content: "Hello World";
+}`;
+
+    const css = shallow(syntaxHighlighter(code, 'css')).html();
+
+    expect(css).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'scss')).html()).toStrictEqual(css);
+    expect(shallow(syntaxHighlighter(code, 'stylus')).html()).toStrictEqual(css);
   });
 
-  it('should work for go', () => {
-    expect(shallow(syntaxHighlighter('func main() {}', 'go')).html()).toContain('cm-variable');
+  it('cURL', () => {
+    const code = `curl --request GET \
+  --url 'https://dash.readme.io/api/v1/api-specification?perPage=10&page=1' \
+  --header 'x-readme-version: v3.0'`;
+
+    expect(shallow(syntaxHighlighter(code, 'curl')).html()).toMatchSnapshot();
   });
 
-  it('should work for powershell', () => {
-    expect(shallow(syntaxHighlighter('$headers.Add("accept", "application/json")', 'powershell')).html()).toContain(
-      'cm-variable'
-    );
+  it('dart', () => {
+    const code = `main() {
+  print('Hello World');
+}`;
+
+    expect(shallow(syntaxHighlighter(code, 'dart')).html()).toMatchSnapshot();
   });
 
-  it('should work for typescript', () => {
-    expect(shallow(syntaxHighlighter('let { a, b }: { a: string, b: number } = o;', 'typescript')).html()).toContain(
-      'cm-variable'
-    );
+  it('dockerfile', () => {
+    const code = `FROM alping:3.4`;
+    expect(shallow(syntaxHighlighter(code, 'dockerfile')).html()).toMatchSnapshot();
   });
 
-  it('should work for swift', () => {
-    expect(shallow(syntaxHighlighter('var x = 0;', 'swift')).html()).toContain('cm-def');
+  it('go', () => {
+    const code = `package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+}`;
+
+    expect(shallow(syntaxHighlighter(code, 'go')).html()).toMatchSnapshot();
+  });
+
+  it('html', () => {
+    const code = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+  </body>
+  </html>`;
+
+    const html = shallow(syntaxHighlighter(code, 'html')).html();
+
+    expect(html).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'xhtml')).html()).toMatchSnapshot(); // @fix this
+  });
+
+  it('java', () => {
+    const code = `public class Java {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}`;
+
+    expect(shallow(syntaxHighlighter(code, 'java')).html()).toMatchSnapshot();
+  });
+
+  describe('javascript', () => {
+    it('should highlight', () => {
+      const code = `console.log("Hello World");`;
+      const javascript = shallow(syntaxHighlighter(code, 'javascript')).html();
+
+      expect(javascript).toMatchSnapshot();
+      expect(shallow(syntaxHighlighter(code, 'ecmascript')).html()).toStrictEqual(javascript);
+      expect(shallow(syntaxHighlighter(code, 'js')).html()).toStrictEqual(javascript);
+      expect(shallow(syntaxHighlighter(code, 'node')).html()).toStrictEqual(javascript);
+      expect(shallow(syntaxHighlighter(code, 'typescript')).html()).toStrictEqual(javascript);
+    });
+
+    it('should highlight typescript', () => {
+      const code = `let { a, b }: { a: string, b: number } = o;`;
+
+      expect(shallow(syntaxHighlighter(code, 'typescript')).html()).toContain('cm-variable');
+    });
+  });
+
+  it('json', () => {
+    const code = `{ "hello": "world" }`;
+    expect(shallow(syntaxHighlighter(code, 'json')).html()).toMatchSnapshot();
+  });
+
+  it('kotlin', () => {
+    const code = `package hello
+
+fun main() {
+  println("Hello World")
+}`;
+
+    const kotlin = shallow(syntaxHighlighter(code, 'kotlin')).html();
+
+    expect(kotlin).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'kt')).html()).toStrictEqual(kotlin);
+  });
+
+  it('liquid', () => {
+    const code = `{% if user %}
+  Hello {{ user.name }}!
+{% endif %}`;
+
+    expect(shallow(syntaxHighlighter(code, 'liquid')).html()).toMatchSnapshot();
+  });
+
+  it('markdown', () => {
+    const code = `# Hello World`;
+    expect(shallow(syntaxHighlighter(code, 'markdown')).html()).toMatchSnapshot();
+  });
+
+  it('objective-c', () => {
+    const code = `/*
+ Build on OS X:
+ clang -framework Foundation -fobjc-arc objc.m -o objc
+
+ Build on Linux with GNUstep:
+ clang \`gnustep-config --objc-flags\` \`gnustep-config --base-libs\` -fobjc-nonfragile-abi -fobjc-arc objc.m -o objc
+ */
+
+#import <Foundation/Foundation.h>
+
+int main(void)
+{
+    NSLog(@"Hello World");
+}`;
+
+    const objc = shallow(syntaxHighlighter(code, 'objc')).html();
+
+    expect(objc).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'objective-c')).html()).toStrictEqual(objc);
+    expect(shallow(syntaxHighlighter(code, 'objectivec')).html()).toStrictEqual(objc);
+  });
+
+  describe('php', () => {
+    it('should highlight', () => {
+      const code = `<?php
+
+echo 'Hello World';`;
+
+      expect(shallow(syntaxHighlighter(code, 'php')).html()).toMatchSnapshot();
+    });
+
+    it('should highlight if missing an opening `<?php` tag', () => {
+      expect(shallow(syntaxHighlighter('echo "Hello World";', 'php')).html()).toContain('cm-keyword');
+    });
+  });
+
+  it('powershell', () => {
+    const code = `'Hello World'`;
+    expect(shallow(syntaxHighlighter(code, 'powershell')).html()).toMatchSnapshot();
+  });
+
+  it('python', () => {
+    const code = `#!/usr/bin/env python
+print "Hello World"`;
+
+    const python = shallow(syntaxHighlighter(code, 'python')).html();
+
+    expect(python).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'py')).html()).toStrictEqual(python);
+  });
+
+  it('ruby', () => {
+    const code = `#!/usr/bin/env ruby
+puts "Hello World"`;
+
+    const ruby = shallow(syntaxHighlighter(code, 'ruby')).html();
+
+    expect(ruby).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'jruby')).html()).toStrictEqual(ruby);
+    expect(shallow(syntaxHighlighter(code, 'macruby')).html()).toStrictEqual(ruby);
+    expect(shallow(syntaxHighlighter(code, 'rake')).html()).toStrictEqual(ruby);
+    expect(shallow(syntaxHighlighter(code, 'rb')).html()).toStrictEqual(ruby);
+    expect(shallow(syntaxHighlighter(code, 'rbx')).html()).toStrictEqual(ruby);
+  });
+
+  it('scala', () => {
+    const code = `object HelloWorld extends App {
+  println("Hello World")
+}`;
+
+    expect(shallow(syntaxHighlighter(code, 'scala')).html()).toMatchSnapshot();
+  });
+
+  it('shell', () => {
+    const code = `#!/bin/sh
+echo "Hello World"`;
+
+    const shell = shallow(syntaxHighlighter(code, 'shell')).html();
+
+    expect(shell).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'bash')).html()).toStrictEqual(shell);
+    expect(shallow(syntaxHighlighter(code, 'sh')).html()).toStrictEqual(shell);
+    expect(shallow(syntaxHighlighter(code, 'zsh')).html()).toStrictEqual(shell);
+  });
+
+  it('sql', () => {
+    const code = `SELECT 'Hello World';`;
+
+    const sql = shallow(syntaxHighlighter(code, 'sql')).html();
+
+    expect(sql).toMatchSnapshot();
+    expect(shallow(syntaxHighlighter(code, 'cql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'mssql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'mysql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'plsql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'postgres')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'postgresql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'pgsql')).html()).toStrictEqual(sql);
+    expect(shallow(syntaxHighlighter(code, 'sqlite')).html()).toStrictEqual(sql);
+  });
+
+  it('swift', () => {
+    const code = `print("Hello World")`;
+
+    expect(shallow(syntaxHighlighter(code, 'swift')).html()).toMatchSnapshot();
+  });
+
+  it('xml', () => {
+    const code = `<?xml version="1.0" encoding="UTF-8"?>
+<text><![CDATA[Hello World]]></text>`;
+
+    expect(shallow(syntaxHighlighter(code, 'xml')).html()).toMatchSnapshot();
   });
 });
