51cafb94ebbdf277541b62f1cf5246a09a8e1c17
Gabriel Ratcliff <gaberatcliff@gmail.com>
2020-05-05 12:47:44 -0700

    Reintegrating legacy kebab into id generation

--- packages/markdown/processor/plugin/section-anchor-id.js
+++ packages/markdown/processor/plugin/section-anchor-id.js
@@ -1,12 +1,12 @@
 const flatMap = require('unist-util-flatmap');
-// const kebab = require('lodash/kebabCase');
+const kebabCase = require('lodash/kebabCase');
 
 // Adds an empty <div id='section-slug'> next to all headings.
 // This is for compatibility with how we used to do slugs
 function transformer(ast) {
   return flatMap(ast, node => {
     if (node.type === 'element' && node.properties && node.properties.id) {
-      const id = `section-${node.properties.id}`;
+      const id = `section-${kebabCase(node.properties.id)}`;
       return [node, { type: 'element', tagName: 'div', properties: { id } }];
     }
     return [node];
