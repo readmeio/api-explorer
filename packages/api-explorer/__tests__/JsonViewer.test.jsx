9a9a69cee5cc1972fcfdccc1b8ce1aeb4a79846f
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2020-05-20 17:55:11 +0200

    feat(MMAP-11): expand/collapse schemas (#53)

--- packages/api-explorer/__tests__/JsonViewer.test.jsx
+++ packages/api-explorer/__tests__/JsonViewer.test.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl';
-import { mountWithIntl } from 'enzyme-react-intl'
+import ReactTestUtils from 'react-dom/test-utils'
+import { mountWithIntl, loadTranslationObject } from 'enzyme-react-intl'
 import ReactJson from 'react-json-view'
+import { Button } from 'antd';
 
+import strings from '../i18n/en.json'
 import JsonViewer from '../src/components/JsonViewer';
 
 describe('JsonViewer', () => {
+  loadTranslationObject({
+    ...strings,
+    'foo.id': 'missing schema'
+  })
+
   test('if schema is defined renders ReactJson', () => {
     const props = {
       schema: {
@@ -17,10 +25,10 @@ describe('JsonViewer', () => {
     const reactJson = element.find(ReactJson)
     expect(reactJson).toHaveLength(1)
     expect(reactJson.prop('src')).toEqual(props.schema)
-    expect(element.html()).toMatchSnapshot()
+    expect(element.getDOMNode()).toMatchSnapshot()
   })
 
-  test('if schema is defined renders ReactJson', () => {
+  test('if schema is undefined renders missing message', () => {
     const props = {
       missingMessage: 'foo.id'
     }
@@ -29,4 +37,68 @@ describe('JsonViewer', () => {
     expect(formattedMessage).toHaveLength(1)
     expect(formattedMessage.prop('id')).toEqual(props.missingMessage)
   })
+
+  describe('Expand - Collapse button', () => {
+    const schema = {
+      foo: {
+        bar: {
+          lorem: 'ipsum'
+        },
+        something: {
+          else: {
+            else: {
+              else: 'example'
+            }
+          }
+        }
+      }
+    }
+    let element
+    beforeEach(() => {
+      element = mountWithIntl(<JsonViewer schema={schema} missingMessage={'missing'} />)
+    })
+
+    test('is collapsed as default', () => {
+      checkCollapse(element, {isCollapsed: true})
+
+      expect(element.getDOMNode()).toMatchSnapshot()
+    })
+
+    test('expand when click button', () => {
+      clickExpandButton(element)
+      checkCollapse(element, {isCollapsed: false})
+
+      expect(element.getDOMNode()).toMatchSnapshot()
+    })
+
+    test('flow', () => {
+      checkCollapse(element, {isCollapsed: true})
+
+      clickExpandButton(element)
+      checkCollapse(element, {isCollapsed: false})
+      
+      clickExpandButton(element)
+      checkCollapse(element, {isCollapsed: true})
+      
+      clickExpandButton(element)
+      checkCollapse(element, {isCollapsed: false})
+    })
+  })
 })
+
+function clickExpandButton(element) {
+  ReactTestUtils.act(() => {
+    element.find(Button).prop('onClick')()
+  })
+  element.update()
+}
+
+function checkCollapse(element, {isCollapsed}) {
+  if (isCollapsed) {
+    expect(element.find(ReactJson).prop('collapsed')).toEqual(1)
+    expect(element.find(Button).find(FormattedMessage).prop('id')).toEqual('schemas.expand')
+    return
+  }
+  expect(element.find(ReactJson).prop('collapsed')).toEqual(false)
+  expect(element.find(Button).find(FormattedMessage).prop('id')).toEqual('schemas.collapse')
+}
--- packages/api-explorer/src/components/JsonViewer/index.jsx
+++ packages/api-explorer/src/components/JsonViewer/index.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
+/* eslint-disable react/require-default-props */
+import React, {useState} from 'react'
 import { FormattedMessage } from 'react-intl'
+import {Button} from 'antd'
 import PropTypes from 'prop-types'
 import ReactJson from 'react-json-view'
 
 import colors from '../../colors'
 
+const collapseButtonStyle = {
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  zIndex: 99999
+}
+
 export default function JsonViewer({ schema, missingMessage }) {
+  const [isCollapsed, setIsCollapsed] = useState(true)
+
   return (
-    schema ? <ReactJson
-      src={schema}
-      collapsed={1}
-      collapseStringsAfterLength={100}
-      enableClipboard={false}
-      name={null}
-      displayDataTypes={false}
-      displayObjectSize={false}
-      style={{
-        padding: '20px 10px',
-        backgroundColor: colors.jsonViewerBackground,
-        fontSize: '12px',
-        overflow: 'visible',
-        whiteSpace: 'pre-wrap',
-        wordBreak: 'break-all'
-      }}
-    /> : <FormattedMessage id={missingMessage} />
+    schema ? (
+      <div style={{position: 'relative'}}>
+        <Button style={collapseButtonStyle} onClick={() => setIsCollapsed(!isCollapsed)}>
+          <FormattedMessage id={`schemas.${isCollapsed ? 'expand' : 'collapse'}`} />
+        </Button>
+        <ReactJson
+          src={schema}
+          collapsed={isCollapsed ? 1 : false}
+          collapseStringsAfterLength={100}
+          enableClipboard={false}
+          name={null}
+          displayDataTypes={false}
+          displayObjectSize={false}
+          style={{
+            padding: '20px 10px',
+            backgroundColor: colors.jsonViewerBackground,
+            fontSize: '12px',
+            overflow: 'visible',
+            whiteSpace: 'pre-wrap',
+            wordBreak: 'break-all'
+          }}
+        />
+      </div>
+    ) : <FormattedMessage id={missingMessage} />
   )
 }
 
 JsonViewer.propTypes = {
-  schema: PropTypes.object.isRequired,
+  schema: PropTypes.object,
   missingMessage: PropTypes.string.isRequired
 }
