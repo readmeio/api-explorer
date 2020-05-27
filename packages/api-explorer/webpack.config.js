f22e4b16f5facf32c0d3e0c16a1d62c365797926
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2020-05-27 18:32:03 +0200

    fix: exclude react from build (#72)

--- packages/api-explorer/webpack.config.js
+++ packages/api-explorer/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
       },
     ],
   },
+  externals: {
+    react: 'react',
+    reactDOM: 'react-dom'
+  },
   output: {
     filename: './dist/index.js',
     libraryTarget: 'commonjs2',
