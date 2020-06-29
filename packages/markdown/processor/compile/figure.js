1e12e0c7965dd5327e074dc1e49d6dfc7aa2af83
Rafe Goldberg <rafegoldberg@gmail.com>
2020-06-29 07:44:06 -0700

    refactor(markdown): figcaption compiler
    split `figcaption` compiler out of the `figure` visitor methods

--- packages/markdown/processor/compile/figure.js
+++ packages/markdown/processor/compile/figure.js
@@ -4,15 +4,18 @@ module.exports = function FigureCompiler() {
   const { Compiler } = this;
   const { visitors } = Compiler.prototype;
 
+  visitors.figcaption = function figcaptionCompiler(node) {
+    const caption = nodeToString(node);
+    return `<figcaption>${caption}</figcaption>`;
+  };
+
   visitors.figure = function figureCompiler(node) {
     const [image, caption] = node.children;
     return `<figure>
 
-  ${visitors.image.call(this, image)}
+${visitors.image.call(this, image)}
 
-  <figcaption>
-    ${nodeToString(caption)}
-  </figcaption>
+${visitors.figcaption.call(this, caption)}
 </figure>`;
   };
 };
