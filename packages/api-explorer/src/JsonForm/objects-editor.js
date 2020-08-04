84ead754dbd22eca79f774950b36828bc75eeba9
Filippo Rezzonico <filippo1.rezzonico@mail.polimi.it>
2020-08-04 11:05:07 +0200

    FIX(BMP-418): API explorer modal positioning bug correction (#85)

--- packages/api-explorer/src/JsonForm/objects-editor.js
+++ packages/api-explorer/src/JsonForm/objects-editor.js
@@ -25,7 +25,7 @@ module.exports = (classReference) => class objectCustomEditor extends classRefer
       this.editjson_textarea.style.width = '450px'
       this.editjson_textarea.style.height = '340px'
       this.editjson_textarea.style.fontFamily = 'monospace'
-      const leftSize = this.addproperty_button.offsetWidth - this.editjson_holder.offsetWidth + parseInt(this.addproperty_button.style.marginLeft, 10) + parseInt(this.addproperty_button.style.marginRight, 10)
+      const leftSize = parseInt(this.editjson_holder.style.left, 10) - this.editjson_holder.offsetWidth - parseInt(this.editjson_control.style.marginLeft, 10)
       if(leftSize && typeof leftSize === 'number' && !isNaN(leftSize)){
         this.editjson_holder.style.left = `${leftSize}px`
       }
