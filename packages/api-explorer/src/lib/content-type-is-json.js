e1dac437b186ece22f2f6581b85fe412515b1fdc
Jon Ursenbach <erunion@users.noreply.github.com>
2020-11-09 17:14:36 -0800

    chore(deps): upgrading oas to 5.2.1 (#1040)

--- packages/api-explorer/src/lib/content-type-is-json.js
+++ packages/api-explorer/src/lib/content-type-is-json.js
@@ -1,5 +1,5 @@
 function contentTypeIsJson(contentType) {
-  const jsonContentTypes = ['application/json', '+json'];
+  const jsonContentTypes = ['application/json', 'application/x-json', 'text/json', 'text/x-json', '+json', '*/*'];
   return jsonContentTypes.some(ct => contentType.includes(ct));
 }
 
