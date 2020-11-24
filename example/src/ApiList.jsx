a8a9e4307676b2866a6881708a6f54d469d9e06d
Jon Ursenbach <jon@ursenba.ch>
2020-11-24 13:39:57 -0600

    style: incorporating ui components in the preview site

--- example/src/ApiList.jsx
+++ example/src/ApiList.jsx
@@ -1,6 +1,7 @@
 const { stringify, parse } = require('querystring');
 const React = require('react');
 const PropTypes = require('prop-types');
+const { Button, Flex, Input, Select, Title } = require('@readme/ui/.bundles/es/ui/components');
 
 const localDirectory = require('../swagger-files/directory.json');
 
@@ -49,40 +50,47 @@ class ApiList extends React.Component {
   render() {
     const { apis, selected, customUrl } = this.state;
 
-    return (
-      <h3>
-        Select an API:&nbsp;
-        {selected === 'enter-a-url' ? (
-          <form style={{ display: 'inline' }}>
-            <input name="selected" type="url" />
-            <input name="customUrl" type="hidden" value="true" />
-            <button type="submit">Go</button>
-          </form>
-        ) : (
-          <>
-            <select onChange={this.changeApi} value={selected}>
-              <option value="enter-a-url">Enter a URL</option>
-              {selected && customUrl && (
-                <option disabled value={selected}>
-                  {selected}
-                </option>
-              )}
+    const options = [{ label: 'Enter a URL', value: 'enter-a-url' }];
 
-              {Object.keys(apis).map(name => {
-                const api = apis[name];
-                const preferred = api.preferred || Object.keys(api.versions)[0];
-                return (
-                  <option key={name} value={api.versions[preferred].swaggerUrl}>
-                    {name.substring(0, 30)}
-                  </option>
-                );
-              })}
-            </select>
-            &nbsp;
-            <a href={selected}>View document</a>
-          </>
-        )}
-      </h3>
+    if (selected && customUrl) {
+      options.push({
+        label: selected,
+        disabled: true,
+        value: selected,
+      });
+    }
+
+    Object.keys(apis).forEach(name => {
+      const api = apis[name];
+      const preferred = api.preferred || Object.keys(api.versions)[0];
+
+      options.push({
+        label: name,
+        value: api.versions[preferred].swaggerUrl,
+      });
+    });
+
+    return (
+      <Flex>
+        <Title level={4}>
+          Select an API:
+          {selected === 'enter-a-url' ? (
+            <form style={{ display: 'inline' }}>
+              <Input name="selected" size="sm" type="url" />
+              <Input name="customUrl" type="hidden" value="true" />
+              <Button bem={{ slate: true }} size="sm" type="submit">
+                Go
+              </Button>
+            </form>
+          ) : (
+            <React.Fragment>
+              <Select onChange={this.changeApi} options={options} size="sm" value={selected} />
+              &nbsp;
+              <a href={selected}>View document</a>
+            </React.Fragment>
+          )}
+        </Title>
+      </Flex>
     );
   }
 }
