2614ec4aa5cf6879795e86bfad950f4f4ae12630
Jon Ursenbach <erunion@users.noreply.github.com>
2020-12-04 22:41:25 -0800

    chore: deplatforming @readme/markdown-magic from this repo (#1083)

--- packages/markdown-magic/jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('../../jest.config');
-
-module.exports = {
-  ...config,
-  rootDir: './',
-  transformIgnorePatterns: ['/node_modules/(?!@readme/syntax-highlighter).+\\.js$'],
-};
