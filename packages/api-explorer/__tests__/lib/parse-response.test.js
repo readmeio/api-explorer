f7e8faf20aa8f87442b99e33911332c9d3ca926d
Dom Harrington <domharrington@protonmail.com>
2021-02-03 14:04:02 -0800

    chore: add missing test for the wildcard response type branch
    The condition at the end of this line didn't have a test:
    https://github.com/readmeio/api-explorer/blob/77b90ebed4673f168354cdcd730e34b7ee016360/packages/api-explorer/src/lib/parse-response.js#L15

--- packages/api-explorer/__tests__/lib/parse-response.test.js
+++ packages/api-explorer/__tests__/lib/parse-response.test.js
@@ -208,3 +208,13 @@ test('should not error if invalid json is returned', async () => {
 
   expect((await parseResponse(har, invalidJsonResponse)).responseBody).toBe('plain text');
 });
+
+test('should default to JSON with wildcard content-type', async () => {
+  const wildcardResponse = new Response(responseBody, {
+    headers: {
+      'Content-Type': '*/*',
+    },
+  });
+
+  expect((await parseResponse(har, wildcardResponse)).responseBody).toStrictEqual(JSON.parse(responseBody));
+});
