de423767718255dea848e836c45e63bd57dce46d
Jon Ursenbach <jon@ursenba.ch>
2021-02-05 14:51:02 -0800

    ci: more tinkering with lhci

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,7 @@ module.exports = {
       url: ['http://localhost:9966/'],
       startServerCommand: 'npm start',
       startServerReadyPattern: 'Compiled successfully.',
+      startServerReadyTimeout: 20000,
       // settings: {
       //   emulatedFormFactor: 'desktop',
       //   disableDeviceEmulation: true,
@@ -15,7 +16,7 @@ module.exports = {
     upload: {
       target: 'lhci',
       serverBaseUrl: process.env.LHCI_URL,
-      token: process.env.LHCI_TOKEN,
+      token: '8a40d86e-1b1d-4e69-b852-4eec88c37ffd', // process.env.LHCI_TOKEN,
       basicAuth: {
         username: process.env.LHCI_USERNAME,
         password: process.env.LHCI_PASSWORD,
