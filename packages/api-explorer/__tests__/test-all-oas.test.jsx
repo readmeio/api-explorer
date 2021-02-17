062fdeeeca9e96e425a75ff77e9547d101d0e27e
Aaron Hedges <aaron@readme.io>
2021-02-16 19:57:17 -0500

    feat(api-explorer): tests now scan all example json, including manual tests

--- packages/api-explorer/__tests__/test-all-oas.test.jsx
+++ packages/api-explorer/__tests__/test-all-oas.test.jsx
@@ -5,37 +5,59 @@ const createParams = require('../src/Params');
 const fs = require('fs');
 const path = require('path');
 
-const folderPath = '/Users/aaronhedges/oasFileDump';
+const folderPath = path.join(__dirname, '../../../example/swagger-files');
+// const folderPath = '/Users/aaronhedges/oasFileDump';
 const dir = fs.readdirSync(folderPath);
 
-test('should load fine', () => {
-  dir.forEach(oasFilename => {
-    const oasFile = fs.readFileSync(path.join(folderPath, oasFilename));
-    const oas = new Oas(oasFile);
-    const operation = oas.operation('/pet/{petId}', 'get');
+function testOasObject(oas, oasFile) {
+  Object.keys(oasFile.paths).forEach(path => {
+    Object.keys(oasFile.paths[path]).forEach(method => {
+      if (!['get', 'post', 'put', 'patch', 'delete'].includes(method)) {
+        return;
+      }
 
-    const props = {
-      oas,
-      onChange: () => {},
-      onJsonChange: () => {},
-      onModeChange: () => {},
-      onSubmit: () => {},
-      operation,
-      resetForm: () => {},
-      onError: e => {
-        console.log('error');
-        throw e;
-      },
-    };
+      console.log('checking ' + path + ' ' + method);
+      const operation = oas.operation(path, method);
+      const props = {
+        oas,
+        onChange: () => {},
+        onJsonChange: () => {},
+        onModeChange: () => {},
+        onSubmit: () => {},
+        operation,
+        resetForm: () => {},
+        onError: e => {
+          console.log('error');
+          throw e;
+        },
+      };
 
-    const Params = createParams(oas, operation);
-    let params;
+      const Params = createParams(oas, operation);
+      let params;
 
-    try {
       params = mount(React.createElement(Params, props));
-    } catch (e) {
-      console.log(`file ${oasFilename} failed`);
-    }
-    expect(params).toBeDefined();
+      expect(params.html()).not.toContain('currently experiencing difficulties');
+    });
   });
+}
+
+test('should load fine', async () => {
+  for (const oasFilename of dir) {
+    console.log('checking ' + oasFilename);
+    const oasFile = JSON.parse(fs.readFileSync(path.join(folderPath, oasFilename)));
+    let oas = null;
+
+    if (oasFile.paths) {
+      oas = new Oas(oasFile);
+      await oas.dereference();
+      testOasObject(oas, oasFile);
+    } else if (oasFile.oasFiles) {
+      for (const key of Object.keys(oasFile.oasFiles)) {
+        console.log('checking ' + key);
+        oas = new Oas(oasFile.oasFiles[key]);
+        await oas.dereference();
+        testOasObject(oas, oasFile.oasFiles[key]);
+      }
+    }
+  };
 });
