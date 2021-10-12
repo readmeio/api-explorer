f6a7d01495a640a6c584081307fb64fdb1685c26
giovanni tommasi <giovannitommasi95@gmail.com>
2021-10-12 16:42:33 +0200

    typo

--- packages/api-explorer/__tests__/AuthBox.test.jsx
+++ packages/api-explorer/__tests__/AuthBox.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl';
 import { shallowWithIntl, mountWithIntl } from 'enzyme-react-intl';
 
 import AuthBox from '../src/AuthBox';
